381782e52819fc158ee338600690561fd32da275
Kevin <kevinallenoneill@gmail.com>
2018-07-09 19:01:04 -0500

    changes to grid

--- src/Helpers.js
+++ src/Helpers.js
@@ -4,7 +4,7 @@ export function moveBack(c){
  var margin = parseInt($('.'+ c +' .grid').css('margin-left'));
 
  if(margin < 0){
-    margin += 217.5;
+    margin += 280;
     $('.'+ c +' .grid').animate({marginLeft: [margin, "swing"]}, 200);
     if(margin > 0){
         $('.'+ c +' .grid').css('margin-left', '0')
@@ -16,15 +16,15 @@ export function moveBack(c){
 
 export function moveForward(c){
     var gridLength = $('.'+ c +' .grid .grid-item').length -3;
-
-    var width = gridLength * (213.5 + 4);
+    console.log($('.grid-item').width())
+    var width = gridLength * (280 + 4);
     var margin = parseInt($('.'+ c +' .grid').css('margin-left'));
 
     var divWidth = -width;
 
 
     if(margin <= 0 && margin >= divWidth){
-        margin -= 217.5;
+        margin -= 280;
         $('.'+ c +' .grid').animate({marginLeft: [margin, "swing"]}, 200);
     }
 
