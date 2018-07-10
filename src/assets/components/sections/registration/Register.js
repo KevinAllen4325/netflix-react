36d6c708b82c7242f000eb8f107c3fc7246e0ab7
Kevin <kevinallenoneill@gmail.com>
2018-07-10 17:41:52 -0500

    added fade

--- src/assets/components/sections/registration/Register.js
+++ src/assets/components/sections/registration/Register.js
@@ -4,6 +4,7 @@ import Slide1 from './slides/Slide1'
 import Slide2 from './slides/Slide2'
 import Slide3 from './slides/Slide3'
 import Slide4 from './slides/Slide4'
+import Fade from 'react-reveal/Fade';
 class Registration extends React.Component{
     state = {
         step: 1,
@@ -40,18 +41,30 @@ class Registration extends React.Component{
                 return <Slide1
                     nextStep={this.nextStep}/>;
             case 2:
-                return <Slide2
-                    nextStep={this.nextStep}
-                    plan={this.plan}/>;
+                return(
+                    <Fade>
+                        <Slide2
+                            nextStep={this.nextStep}
+                            plan={this.plan}/>
+                    </Fade>
+                );
             case 3:
-                return <Slide3
-                    nextStep={this.nextStep}
-                    account={this.accountInfo}
-                    state={this.state.account}
-                    />;
+                return(
+                    <Fade>
+                        <Slide3
+                            nextStep={this.nextStep}
+                            account={this.accountInfo}
+                            state={this.state.account}
+                        />;
+                    </Fade>
+                )
             case 4:
-                return <Slide4 name={this.state.account.firstName}
-                               plan={this.state.plan}/>;
+                return(
+                    <Fade>
+                        <Slide4 name={this.state.account.firstName}
+                                plan={this.state.plan}/>;
+                    </Fade>
+                )
         }
     };
 
