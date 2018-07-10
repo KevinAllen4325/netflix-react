df1717cd7dd2a8fc9542923a5a3bbb4330781341
Kevin <kevinallenoneill@gmail.com>
2018-07-10 17:09:28 -0500

    form changes

--- src/assets/components/sections/registration/slides/Slide3.js
+++ src/assets/components/sections/registration/slides/Slide3.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 class Slide3 extends React.Component{
-    constructor(props){
-        super(props);
-        this.firstName = React.createRef();
-        this.lastName = React.createRef();
-        this.email = React.createRef();
-        this.password = React.createRef();
-    }
+    firstName = React.createRef();
+    lastName = React.createRef();
+    email = React.createRef();
+    password = React.createRef();
 
     submitForm = (e) =>{
         e.preventDefault();
 
-        // const info = {
-        //     firstName: this.firstName.value.value,
-        //     lastName: this.lastName.value.value,
-        //     email: this.email.value.value,
-        //     password: this.password.value.value,
-        // };
+        const info = {
+            firstName: this.firstName.value,
+            lastName: this.lastName.value,
+            email: this.email.value,
+            password: this.password.value,
+        };
 
-        // this.props.accountInfo(info);
-        // console.log(info);
+        this.props.account(info);
         this.props.nextStep();
 
     };
@@ -32,10 +27,10 @@ class Slide3 extends React.Component{
                 <p className="headline">Sign up to start your free month</p>
                 <p className="dl"><strong>Create your Account</strong></p>
                 <form onSubmit={this.submitForm}>
-                    <input type="text" name="firstName" placeholder="First Name" ref={this.firstName}  required/>
-                    <input type="text" name="lastName" placeholder="Last Name" ref={this.lastName}  required/>
-                    <input type="email" name="email" placeholder="Email" ref={this.email}  required/>
-                    <input type="password" name="password" placeholder="Password" ref={this.password} required/>
+                    <input type="text" name="firstName" placeholder="First Name" ref={(input) => this.firstName = input}  required/>
+                    <input type="text" name="lastName" placeholder="Last Name" ref={(input) => this.lastName = input}  required/>
+                    <input type="email" name="email" placeholder="Email" ref={(input) => this.email = input}  required/>
+                    <input type="password" name="password" placeholder="Password" ref={(input) => this.password = input} required/>
                     <button type="submit" className="seePlans continue">CONFIRM</button>
                 </form>
             </div>
